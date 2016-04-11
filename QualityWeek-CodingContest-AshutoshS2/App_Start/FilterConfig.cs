7efeb20fd15121a81e2cb17721bb61d7c7c9c352
Ashutosh Singh <ashutoshsingh1989@gmail.com>
2016-04-11 10:36:03 +0530

    fd

---
+++ QualityWeek-CodingContest-AshutoshS2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace QualityWeek_CodingContest_AshutoshS2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
