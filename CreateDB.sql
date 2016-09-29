54e5bcd2ae6e65749aff3de027789ec48831686f
Ashutosh Singh <ashutosh.singh@sjsu.edu>
2016-09-29 09:42:05 -0700

    Create CreateDB.sql

---
+++ CreateDB.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE CMPE273;
+CREATE TABLE expenses (name VARCHAR(30), email VARCHAR(30),category VARCHAR(20),descriptiom VARCHAR(50),link VARCHAR(50),
+                       estimated_costs DECIMAL, submit_date DATE, status VARCHAR(20),decision_date DATE);
